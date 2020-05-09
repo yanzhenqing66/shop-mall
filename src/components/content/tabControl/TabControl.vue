8c5d6a8e322de9f93d78cd0512d3df606a4b14e7
yanzhenqing66 <504767693@qq.com>
2020-05-09 21:01:41 +0800

    封装选项卡控件

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tab-control">
+    <div class="tab-control-item" v-for="(item, index) in titles" :class="{active: index == currentIndex}" @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    titles: Array,
+    default() {
+      return []
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+    }
+  },
+}
+</script>
+
+<style>
+  .tab-control {
+    display: flex;
+    height: 44px;
+    font-size: 16px;
+    text-align: center;
+    background-color: #fff;
+  }
+
+  .tab-control-item {
+    flex: 1;
+    line-height: 44px;
+  }
+
+  .tab-control span {
+    padding: 5px;
+  }
+
+  .active {
+    color: var(--color-high-text);
+  }
+
+  .active span {
+    border-bottom: 2px solid var(--color-tint);
+  }
+
+
+</style>
