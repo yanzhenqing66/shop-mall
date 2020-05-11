ac5619a6c9de1a977ffa4cdcbf092c41155cfc1d
yanzhenqing66 <504767693@qq.com>
2020-05-11 15:31:11 +0800

    初始化项目

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ module.exports = {
     resolve: {
       alias: {
         'assets': '@/assets',
-        'common': '@/common',
         'components': '@/components',
         'network': '@/network',
         'views': '@/views'
