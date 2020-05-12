d6a799be9d36dd6b5f3de5328beac8a26b2cbd70
yanzhenqing66 <504767693@qq.com>
2020-05-12 13:46:19 +0800

    监听图片加载和防抖函数

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -12,8 +12,14 @@ import BScroll from "better-scroll";
 export default {
   name: "Scroll",
   props: {
-    probeType: Number,
-    default: 0
+    probeType: {
+      type: Number,
+      default: 0
+    },
+    pullUpLoad: {
+      type: Boolean,
+      default: false
+    }
   },
   data() {
     return {
@@ -21,22 +27,36 @@ export default {
     };
   },
   mounted() {
+		// 创建 BScroll 对象
     this.scroll = new BScroll(this.$refs.wrapper, {
       click: true,
-      probeType: this.probeType
+      probeType: this.probeType,
+      pullUpLoad: this.pullUpLoad
     });
-    this.scroll.on('scroll', (position) => {
-      this.$emit('scroll', position)
-    })
+		// 监听滚动位置
+    this.scroll.on("scroll", position => {
+      this.$emit("scroll", position);
+    });
+		// 上滑加载更多
+    this.scroll.on("pullingUp", () => {
+      this.$emit("pullingUp");
+    });
   },
   methods: {
-    scrollTo(x, y, time=300) {
-        this.scroll.scrollTo(x, y, time)
-      },
-  },
+    scrollTo(x, y, time = 300) {
+      this.scroll && this.scroll.scrollTo(x, y, time);
+    },
+    finishPullUp() {
+        this.scroll.finishPullUp()
+    },
+		// 刷新数据
+		refresh() {
+			// console.log('asd')
+			this.scroll && this.scroll.refresh()
+		}
+  }
 };
 </script>
 
 <style scoped>
-
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,7 +3,7 @@
     <nav-bar class="home-nav">
         <div slot="center">购物街</div>
       </nav-bar>
-    <scroll class="content" ref="scroll" :probe-type="3" @scroll="contentScroll">
+    <scroll class="content" ref="scroll" :probe-type="3" @scroll="contentScroll" :pull-up-load="true" @pullingUp="loadMore">
       <home-swiper :banners="banners"></home-swiper>
       <recommond-view :recommends="recommends"></recommond-view>
       <feature-view />
@@ -53,15 +53,36 @@ export default {
     };
   },
   created() {
+    // 请求多个数据
     this.getHomeMultidata();
+    // 请求商品数据
     this.getHomeGoods("pop");
     this.getHomeGoods("new");
     this.getHomeGoods("sell");
   },
+	mounted() {
+		// 监听 goodsListItem 图片加载完成
+		const refresh = this.debounce(this.$refs.scroll.refresh)
+		this.$bus.$on('goodImgLoad', () => {
+			refresh()
+		})
+	},
   methods: {
     /**
      * 事件监听相关的方法
      */
+		
+		// 防抖函数
+		debounce(fn, delay) {
+			let timer = null
+			return function (...args) {
+				if (timer) clearTimeout(timer)
+				timer = setTimeout(() => {
+					fn.apply(this, args)
+				}, delay)
+			}
+		},
+		
     tabClick(index) {
       switch (index) {
         case 0:
@@ -81,6 +102,11 @@ export default {
     contentScroll(position) {
       this.isShowACtive = (-position.y) > 1000
     },
+    loadMore() {
+      this.getHomeGoods(this.currentType)
+
+      // this.$refs.scroll.scroll.refresh()
+    },
 
     /**
      * 网络请求相关的方法
@@ -96,6 +122,8 @@ export default {
       getHomeGoods(type, page).then(res => {
         this.goods[type].list.push(...res.data.list);
         this.goods[type].page += 1;
+        
+        this.$refs.scroll.finishPullUp()
       });
     }
   }
